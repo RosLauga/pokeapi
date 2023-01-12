48ad68c8056b2e87c9ab9929081d1756fdea8038
Roslauga <rodrigo.lauga469u@gmail.com>
2023-01-12 14:22:35 -0300

    fixes data folder and test

--- pokeapi/src/components/searchbar/searchbar.test.js
+++ pokeapi/src/components/searchbar/searchbar.test.js
@@ -1,32 +1,29 @@
-import '@testing-library/jest-dom/extend-expect'
-import { render, screen, fireEvent, } from '@testing-library/react';
-import SearchBar from './searchbar'
-import {  prettyDOM } from '@testing-library/dom';
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen, fireEvent } from "@testing-library/react";
+import SearchBar from "./searchbar";
+import { prettyDOM } from "@testing-library/dom";
 
+describe("test search bar", () => {
+  let component;
+  let mockHandler;
+  beforeEach(() => {
+    mockHandler = jest.fn();
+    component = render(<SearchBar handlerSubmit={mockHandler} />);
+  });
 
-xdescribe("test search bar", () => {
-    let component
-    beforeEach(()=> {
-        mockHandler = jest.fn()
-        component = render(<SearchBar handlerSubmit={mockHandler}/>)
-    } )
+  test("render the component", () => {
+    render(<SearchBar />);
+  });
+  test("must exists a button", () => {
+    const button = component.container.querySelector("button");
+    console.log(prettyDOM(button));
+  });
 
-    test("render the component",()=> {
-    render(<SearchBar />)    
-})
-    test("must exists a button",()=> {
-        
-        const button = component.container.querySelector('button')
-        console.log(prettyDOM(button))
-    });
+  test("must click the button", () => {
+    const buttonclick = component.getByText("Buscar");
 
-    test("must click the button",()=> {
-        
-        const buttonclick = component.getByText('Buscar')
+    fireEvent.submit(buttonclick);
 
-        fireEvent.submit(buttonclick)
-
-        expect(mockHandler.mock.calls).toHaveLength(1)
-
-    })
-})
+    expect(mockHandler.mock.calls).toHaveLength(1);
+  });
+});
